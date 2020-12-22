3d66095a546b9c7ce41684059ab13428c43f14a4
devmesan <mesandraw@gmail.com>
2020-12-22 23:55:32 +0700

    add misc again

--- src/store/index.js
+++ src/store/index.js
@@ -17,16 +17,17 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
-    let store = createStore(
-        persistedReducer,
-        initialState,
-        compose(
-            applyMiddleware(...middleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__
-                ? window.__REDUX_DEVTOOLS_EXTENSION__()
-                : (f) => f
-        )
-    );
+    const devTools =
+        process.env.NODE_ENV === "production"
+            ? applyMiddleware(...middlewares)
+            : compose(
+                  applyMiddleware(...middleware),
+                  window.__REDUX_DEVTOOLS_EXTENSION__
+                      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+                      : (f) => f
+              );
+
+    let store = createStore(persistedReducer, initialState, devTools);
 
     let persistor = persistStore(store);
     return { store, persistor };
