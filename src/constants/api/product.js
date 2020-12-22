2599c29cf5783c4bb01e56b1b0eb84f6cc103d87
devmesan <mesandraw@gmail.com>
2020-12-22 23:26:04 +0700

    pokeworld app

--- src/constants/api/product.js
+++
@@ -1,7 +0,0 @@
-import axios from "../../configs/axios";
-
-/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
-export default {
-    getProduct: () => axios.get("/todos"),
-    saveProduct: (data) => axios.post("/posts", data),
-};
